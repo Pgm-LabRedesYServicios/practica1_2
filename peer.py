cdc0860be4cf7b4170265b100e0bbea8e17919c9
zentauro <pedrogomezmartin1999@gmail.com>
2021-03-16 02:05:11 +0100

    Peer: Document better

--- peer.py
+++ peer.py
@@ -28,16 +28,39 @@ class Server:
 
     def __init__(self, addr: str, port: str):
         self.buff = bytearray(0)
-        self.sock = create_connection((addr, int(port)))
+        try:
+            self.sock = create_connection((addr, int(port)))
+        except ValueError as e:
+            print(f"[x] Error while trying to parse arguments: {e}")
+            sys.exit(EXIT_ERR)
+        except OSError as e:
+            print(f"[x] Error while trying to connect to server: {e.strerror}")
+            sys.exit(EXIT_ERR)
 
-    def get_peers(self) -> list[socket]:
+    def get_peers(self) -> set[socket]:
+        """
+        Once connected to the server, get the connected peers and connect to
+        them
+        """
+        # The first message received is a 4 bytes tag containing the size of
+        # the message
         msg = self.sock.recv(4)
         if len(msg) == 0:
-            print("Connection unexpectedly died")
+            print("[x] Connection unexpectedly died")
             exit(EXIT_ERR)
 
-        size = int.from_bytes(msg, byteorder='big', signed=False)
+        # Count and size are declared here for clarity
+        #
+        # The size is the size received
+        # The count is the bytes received afterwards, used to check if the
+        # received message is complete
         count = 0
+        size = 0
+
+        try:
+            size = int.from_bytes(msg, byteorder='big', signed=False)
+        except ValueError as e:
+            print(f"[x] Error while reading size, corrupted message {e}")
 
         while count != size:
             msg = self.sock.recv(4096)
@@ -47,19 +70,28 @@ class Server:
             else:
                 self.buff += msg
 
+        # If we received less bytes than the server said it was going to send,
+        # we assume that the connection was aborted and terminate
         if count != size:
-            print("Connection unexpectedly died")
+            print("[x] Connection unexpectedly died")
             exit(EXIT_ERR)
 
+        sockets: set[socket] = set()
+
+        # Deserialize the message and connect to the peers sent by the server
         addrs = msgs_capnp.ServerRpcMsg.from_bytes(self.buff)
-        sockets: list[socket] = []
         for addr in addrs.addrs:
             host = ipaddress.IPv4Address(addr.ip).exploded
             port = addr.port
-            print(f"[i] Connecting to {host}:{port}")
-            conn = create_connection((host, port))
-            sockets.append(conn)
 
+            print(f"[i] Connecting to {host}:{port}")
+            try:
+                conn = create_connection((host, port))
+                sockets.add(conn)
+            except OSError as e:
+                print(
+                    f"[-] Error: Cannot connect to {host}:{port}, {e.strerror}"
+                )
 
         return sockets
 
