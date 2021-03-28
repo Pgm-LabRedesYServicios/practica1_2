3f701d5f609f922af3374fd5e5cdb5a9ec1de9b7
zentauro <pedrogomezmartin1999@gmail.com>
2021-03-28 14:42:51 +0200

    Peer: Initial p2p connections impl

--- peer.py
+++ peer.py
@@ -3,8 +3,8 @@
 
 import ipaddress
 import sys
+from select import select
 from socket import AF_INET, SOCK_STREAM, create_connection, socket
-from time import sleep
 
 import capnp
 import msgs_capnp
@@ -13,12 +13,51 @@ EXIT_OK = 0
 EXIT_ERR = 1
 
 incoming_port: int = 0
+counter: int = 0
+
+
+class PeerId():
+    inner: bytearray
+
+    def __init__(self, host: bytes, port: bytes):
+        self.inner = bytearray()
+        self.inner += host
+        self.inner += port
+
+    def __hash__(self):
+        num = int.from_bytes(self.inner, byteorder='big', signed=False)
+        return num
+
+    def __repr__(self):
+        return f"{self.__hash__()}"
+
 
 class Peer:
-    sock: socket
+    in_sock: socket
+    out_sock: socket
+    peer_id: PeerId
 
-    def __init__(self, sock: socket):
-        self.sock = sock
+    def __init__(
+            self,
+            peer_id: PeerId,
+            in_sock: socket = None,
+            out_sock: socket = None
+    ):
+        """
+        Create a peer given the peer_id
+        """
+        self.peer_id = peer_id
+
+        if in_sock is not None:
+            self.in_sock = in_sock
+        if out_sock is not None:
+            self.out_sock = out_sock
+
+    def __hash__(self):
+        return self.peer_id.__hash__()
+
+    def __repr__(self):
+        return f"Peer(id: {self.peer_id.__hash__()})"
 
     def send_msg(self):
         pass
@@ -102,17 +141,50 @@ class Server:
             host = ipaddress.IPv4Address(addr.ip).exploded
             port = addr.port
 
+            peer_id_host = addr.ip.to_bytes(4, byteorder='big', signed=False)
+            peer_id_port = addr.port.to_bytes(2, byteorder='big', signed=False)
+            peer_id = PeerId(peer_id_host, peer_id_port)
+
             print(f"[i] Connecting to {host}:{port}")
             try:
                 conn = create_connection((host, port))
                 conn.send(wire_msg)
-                sockets[conn.fileno()] = Peer(conn)
+                sockets[conn.fileno()] = Peer(peer_id, out_sock=conn)
             except OSError as e:
                 print(
                     f"[-] Error: Cannot connect to {host}:{port}, {e.strerror}"
                 )
 
         return sockets
+
+
+def handle_conn(
+        sock: socket,
+        addr_map: dict[PeerId, Peer],
+        select_map: dict[int, socket]
+):
+    """
+    Handles a new connection, registering its socket and creating its
+    corresponding outbound connection if it doesn't exist
+    """
+    global incoming_port
+    global counter
+
+    msg = sock.recv(4096)
+    host, in_port = sock.getpeername()
+    host_bytes = ipaddress.IPv4Address(host).packed
+    if msg == "":
+        print("[x] Error while reading from new connection")
+    out_port = msgs_capnp.PeerListeningPort.from_bytes(msg).port
+
+    out_id = PeerId(host_bytes, out_port.to_bytes(
+        2, byteorder='big', signed=False
+    ))
+    in_id = PeerId(host_bytes, in_port.to_bytes(
+        2, byteorder='big', signed=False
+    ))
+
+    select_map[sock.fileno()] = sock
 
 
 def main():
@@ -134,10 +206,29 @@ def main():
     # Advertise port
     server.send_port()
     # Get peers
-    peers = server.get_peers()
+    out_peers = server.get_peers()
+
+    # A hashmap from out addr to peer
+    addr_map = {}
+    for peer in out_peers.values():
+        addr_map[peer.peer_id] = peer
+
+    # A hashmap from socketid to socket
+    select_map = {}
+    select_map[sock.fileno()] = sock
+    select_map |= map(lambda k: (k[0], k[1].out_sock), out_peers.items())
 
     while True:
-        sleep(1)
+        selected, x, y = select(select_map, [], [])
+        for s in selected:
+            if s == sock.fileno():
+                conn, (host, port) = sock.accept()
+                print(f"[i] + {host}:{port} Connected")
+                handle_conn(conn, addr_map, select_map)
+            else:
+                pass
+                # f = select_map[s]
+                # handle_msg(f, peers, select_map)
 
 
 if __name__ == "__main__":
