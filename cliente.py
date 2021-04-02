d4ec1cc6b1f20de3c91511ffbc9dda9f8db376cc
zentauro <pedrogomezmartin1999@gmail.com>
2021-04-02 02:33:18 +0200

    Cliente/Servidor: Cosmetic changes and cmd line arguments

--- cliente.py
+++ cliente.py
@@ -8,6 +8,17 @@ messages = [
 ]
 server_address = ('localhost', 10000)
 
+# Parse arguments
+if len(sys.argv) != 3:
+    print("Usage\n\t{sys.argv[0]} <address> <port>")
+
+try:
+    server_address[0] = sys.argv[1]
+    server_address[1] = int(sys.argv[2])
+except ValueError as e:
+    print(f"Error while trying to parse arguments {e}")
+    sys.exit(1)
+
 
 # Create a TCP/IP socket
 socks = [
@@ -15,36 +26,25 @@ socks = [
     socket.socket(socket.AF_INET, socket.SOCK_STREAM),
 ]
 
-
 # Connect the socket to the port where the server is listening
-print('connecting to {} port {}'.format(*server_address),
-    file=sys.stderr)
+print('connecting to {} port {}'.format(*server_address), file=sys.stderr)
 for s in socks:
     s.connect(server_address)
 
-#se envia una parte del mensaje a la vez a traves de cada conector y lee todas las respuestas disposibles despues de escribir nuevos datos
+# Se envia una parte del mensaje a la vez a traves de cada conector y lee todas
+# las respuestas disposibles despues de escribir nuevos datos
 for message in messages:
     outgoing_data = message.encode()
 
     # Send messages on both sockets
     for s in socks:
-        print('{}: sending {!r}'.format(s.getsockname(),
-                                        outgoing_data),
-            file=sys.stderr)
+        print(f"{s.getsockname()}: sending {outgoing_data}", file=sys.stderr)
         s.send(outgoing_data)
 
     # Read responses on both sockets
     for s in socks:
         data = s.recv(1024)
-        print('{}: received {!r}'.format(s.getsockname(),
-                                        data),
-            file=sys.stderr)
+        print(f"{s.getsockname()}: received {data}", file=sys.stderr)
         if not data:
-            print('closing socket', s.getsockname(),
-                file=sys.stderr)
+            print('closing socket', s.getsockname(), file=sys.stderr)
             s.close()
-
-
-
-
-
--- servidor.py
+++ servidor.py
@@ -9,8 +9,18 @@ server.setblocking(0)
 
 # Bind the socket to the port
 server_address = ('', 10000)
-print('starting up on {} port {}'.format(*server_address),
-              file=sys.stderr)
+
+# Parse arguments
+if len(sys.argv) != 2:
+    print("Usage\n\t{sys.argv[0]} <port>")
+
+try:
+    server_address[0] = int(sys.argv[1])
+except ValueError as e:
+    print(f"Error while trying to parse arguments {e}")
+    sys.exit(1)
+
+print(f'starting up on port {server_address[1]}' file=sys.stderr)
 server.bind(server_address)
 
 # Listen for incoming connections
@@ -19,19 +29,14 @@ server.listen(5)
 # Sockets from which we expect to read
 inputs = [server]
 
-
-
-
 #Bucle para bloquear y esperar actividad de la red
 while inputs:
-
-        # Wait for at least one of the sockets to be
-        # ready for processing
+    # Wait for at least one of the sockets to be
+    # ready for processing
     print('waiting for the next event', file=sys.stderr) 
     readable, writable, exceptional = select.select(inputs, [], [])
 
     # Esta seccion establece que el conector del cliente no se bloquee
-
     # Handle inputs
     for s in readable:
         if s is server:
@@ -41,43 +46,19 @@ while inputs:
             connection.setblocking(0)
             inputs.append(connection)
 
-        
-
-
-#los datos se leen con recv(), se colocan  en cola para que puedan ser enviados a traves del conector y de vuelta al cliente
-
+        # Los datos se leen con recv(), se colocan  en cola para que puedan ser
+        # enviados a traves del conector y de vuelta al cliente
         else:
             data = s.recv(1024)
             if data:
                 # A readable client socket has data
-                print('  received {!r} from {}'.format(
-                    data, s.getpeername()), file=sys.stderr,
-                )
+                print(f'  received {data} from {s.getpeername()}', file=sys.stderr)
                 for p in inputs:
-                    if p is not server:
-                        if p is not s:
-                            p.sendall(data)
-                    
-
+                    if p is not server and p is not s:
+                        p.sendall(data)
             else:
                 # Interpret empty result as closed connection
-                print('  closing', client_address,
-                file=sys.stderr)
+                print('  closing', client_address, file=sys.stderr)
                 # Stop listening for input on the connection
                 inputs.remove(s)
                 s.close()
-
-                    #
-
-#Si hay datos en la cola para una conexión, se envía el siguiente mensaje. De otra manera, la conexión se elimina de la lista de conexiones de salida para que la próxima vez a través del bucle select() no indique que el conector está listo para enviar datos.
-
-
-    
-
-#Para acabar, si hya un error con un conector, se cierra
-
-    # Handle "exceptional conditions"
-
-
-
-
