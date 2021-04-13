5de6024c8a435bf3a374fa246cce103c385af0d6
zentauro <pedrogomezmartin1999@gmail.com>
2021-04-14 01:40:50 +0200

    Server/Client: Implement again from lost changes

--- cliente.py
+++ cliente.py
@@ -1,50 +1,55 @@
-import socket
-import sys
-
-messages = [
-    'This is the message. ',
-    'It will be sent ',
-    'in parts.',
-]
-server_address = ('localhost', 10000)
-
-# Parse arguments
-if len(sys.argv) != 3:
-    print("Usage\n\t{sys.argv[0]} <address> <port>")
-
-try:
-    server_address[0] = sys.argv[1]
-    server_address[1] = int(sys.argv[2])
-except ValueError as e:
-    print(f"Error while trying to parse arguments {e}")
-    sys.exit(1)
+from select import select
+from socket import create_connection, socket
+from sys import argv, exit, stdin
 
 
-# Create a TCP/IP socket
-socks = [
-    socket.socket(socket.AF_INET, socket.SOCK_STREAM),
-    socket.socket(socket.AF_INET, socket.SOCK_STREAM),
-]
+def main():
+    # Parse arguments
+    if len(argv) != 3:
+        print(f"Usage\n\t{argv[0]} <address> <port>")
+        exit(1)
 
-# Connect the socket to the port where the server is listening
-print('connecting to {} port {}'.format(*server_address), file=sys.stderr)
-for s in socks:
-    s.connect(server_address)
+    try:
+        # Parse arguments into server address
+        server_address = (argv[1], int(argv[2]))
+        # Try to connect to the server
+        conn = create_connection(server_address)
+    except ValueError as e:
+        print(f"Error while trying to parse arguments {e}")
+        exit(1)
+    except OSError as e:
+        print(f"Failed to connect to server {e}")
+        exit(1)
 
-# Se envia una parte del mensaje a la vez a traves de cada conector y lee todas
-# las respuestas disposibles despues de escribir nuevos datos
-for message in messages:
-    outgoing_data = message.encode()
+    fd_map = {
+        conn.fileno(): conn,
+        stdin.fileno(): stdin
+    }
 
-    # Send messages on both sockets
-    for s in socks:
-        print(f"{s.getsockname()}: sending {outgoing_data}", file=sys.stderr)
-        s.send(outgoing_data)
+    while True:
+        l, m, n = select(fd_map, [], [])
 
-    # Read responses on both sockets
-    for s in socks:
-        data = s.recv(1024)
-        print(f"{s.getsockname()}: received {data}", file=sys.stderr)
-        if not data:
-            print('closing socket', s.getsockname(), file=sys.stderr)
-            s.close()
+        for fd in l:
+            if fd == stdin.fileno():
+                handle_stdin(conn)
+            elif fd == conn.fileno():
+                handle_server(conn)
+
+
+def handle_stdin(server_conn: socket):
+    msg = input()
+    server_conn.send(msg.encode())
+
+
+def handle_server(server_conn: socket):
+    msg = server_conn.recv(4096)
+    if len(msg) == 0:
+        print("The server disconnected")
+        server_conn.close()
+        exit(1)
+
+    print(msg.decode())
+
+
+if __name__ == "__main__":
+    main()
--- servidor.py
+++ servidor.py
@@ -1,64 +1,89 @@
-import select
-import socket
-import sys
+from select import select
+from socket import AF_INET, SOCK_STREAM, socket
+from sys import argv, exit, stderr
 
 
-# Create a TCP/IP socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setblocking(0)
+def setup() -> socket:
+    """
+    Create a socket for the server to listen
+    """
+    # Create a TCP/IP socket
+    server = socket(AF_INET, SOCK_STREAM)
+    server.setblocking(False)
 
-# Bind the socket to the port
-server_address = ('', 10000)
+    # Parse arguments
+    if len(argv) != 2:
+        print(f"Usage\n\t{argv[0]} <port>")
+        exit(1)
 
-# Parse arguments
-if len(sys.argv) != 2:
-    print("Usage\n\t{sys.argv[0]} <port>")
+    try:
+        server_address = ('', int(argv[1]))
+        print(f'starting up on port {server_address[1]}', file=stderr)
+        server.bind(server_address)
+    except ValueError as e:
+        print(f"Error while trying to parse arguments {e}")
+        exit(1)
+    except OSError as e:
+        print(f"Error while trying to bind to {argv[1]}: {e.strerror}")
+        exit(1)
 
-try:
-    server_address[0] = int(sys.argv[1])
-except ValueError as e:
-    print(f"Error while trying to parse arguments {e}")
-    sys.exit(1)
+    # Listen for incoming connections
+    server.listen(5)
 
-print(f'starting up on port {server_address[1]}' file=sys.stderr)
-server.bind(server_address)
+    return server
 
-# Listen for incoming connections
-server.listen(5)
 
-# Sockets from which we expect to read
-inputs = [server]
+def main():
+    server = setup()
 
-#Bucle para bloquear y esperar actividad de la red
-while inputs:
-    # Wait for at least one of the sockets to be
-    # ready for processing
-    print('waiting for the next event', file=sys.stderr) 
-    readable, writable, exceptional = select.select(inputs, [], [])
+    # Sockets from which we expect to read
+    inputs = [server]
 
-    # Esta seccion establece que el conector del cliente no se bloquee
-    # Handle inputs
-    for s in readable:
-        if s is server:
-            # A "readable" socket is ready to accept a connection
-            connection, client_address = s.accept()
-            print('  connection from', client_address, file=sys.stderr) 
-            connection.setblocking(0)
-            inputs.append(connection)
+    # This loop blocks until there is data ready
+    while inputs:
+        # Wait for at least one of the sockets to be ready for processing
+        readable, writable, exceptional = select(inputs, [], [])
 
-        # Los datos se leen con recv(), se colocan  en cola para que puedan ser
-        # enviados a traves del conector y de vuelta al cliente
-        else:
-            data = s.recv(1024)
-            if data:
-                # A readable client socket has data
-                print(f'  received {data} from {s.getpeername()}', file=sys.stderr)
-                for p in inputs:
-                    if p is not server and p is not s:
-                        p.sendall(data)
+        # Handle inputs
+        for s in readable:
+            # A "readable" listening socket is ready to accept a connection
+            if s is server:
+                handle_new(s, inputs)
+
+            # A message incoming from a client
             else:
-                # Interpret empty result as closed connection
-                print('  closing', client_address, file=sys.stderr)
-                # Stop listening for input on the connection
-                inputs.remove(s)
-                s.close()
+                handle_msg(s, inputs, server)
+
+
+def handle_new(s: socket, inputs: list[socket]):
+    """
+    Handles an incoming connection from a client
+    """
+    connection, client_address = s.accept()
+    print('  connection from', client_address, file=stderr)
+    connection.setblocking(False)
+    inputs.append(connection)
+
+
+def handle_msg(s: socket, inputs: list[socket], server: socket):
+    """
+    Handles a message and re transmits it
+    """
+    data = s.recv(1024)
+    addr, port = s.getpeername()
+    if data:
+        # A readable client socket has data
+        print(f'  received {data.decode()} from {addr}:{port}', file=stderr)
+        for p in inputs:
+            if p is not server and p is not s:
+                p.sendall(data)
+    else:
+        # Interpret empty result as closed connection
+        print(f'  closing {addr}:{port}', file=stderr)
+        # Stop listening for input on the connection
+        inputs.remove(s)
+        s.close()
+
+
+if __name__ == "__main__":
+    main()
